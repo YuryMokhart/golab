962e55d7613787b6a6ac8eaecc69d9a4fd6f3d9f
Yury Mokhart <yurymokhart@MacBook-Pro-Yury.local>
2019-06-22 15:38:40 +0300

    Create following: Insert, Find, Delete.

--- user.go
+++ user.go
@@ -6,58 +6,66 @@ import(
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/bson"
 	"context"
-	"time"
+	// "time"
+	"log"
 )
 
 type user struct {
-	id int64
-	name string
-	balance int64
+	ID int64 `json:"ID" bson:"ID"`
+	Name string `json:"Name "bson:"Name"`
+	Balance int64
 }
 
 func main() {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
-		panic(err)
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		log.Fatalf("could not connect mongoDB to a new client: %s\n", err)
+	} 
+
+	ctx := context.Background()
+	// ctx1, close := context.WithTimeout(ctx, 1*time.Second)
+	// defer close()
 
 	err = client.Connect(ctx)
 	if err != nil {
-		panic(err)
+		log.Fatalf("could not initialise the client: %s\n", err)
 	}
-	err = client.Ping(context.TODO(), nil)
+	err = client.Ping(ctx, nil)
 	if err != nil {
-		panic(err)
+		log.Fatalf("could not ping the client: %s\n", err)
 	}
 
 	collection := client.Database("tournament").Collection("user")
 
-	user1 := user{1, "NameUser1", 15}
-	insertResult, err := collection.InsertOne(context.TODO(), user1)
+	user1 := user {
+		ID: 1, 
+		Name: "NameUser1", 
+		Balance: 15,
+	}
+	insertResult, err := collection.InsertOne(ctx, user1)
 	if err != nil {
-	    panic(err)
+	    log.Fatalf("could not insert a user into collection: %s\n", err)
 	}
 	fmt.Println("Inserted document: ", insertResult.InsertedID)
 
-	filter := bson.D{{"name", "NameUser1"}}
+	filter := bson.D{{"Name", "NameUser1"}}
 
 	var result user
 
-	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	err = collection.FindOne(ctx, filter).Decode(&result)
 	if err != nil {
-	    panic(err)
+	    log.Fatalf("could not find a user in the collection: %s\n", err)
 	}
 	fmt.Printf("Found a single document: %+v\n", result)
 
-	deleteResult, err := collection.DeleteOne(context.TODO(), filter)
+	deleteResult, err := collection.DeleteOne(ctx, filter)
 	if err != nil {
-		panic(err)
+		log.Fatalf("could not delete a user from the collection: %s\n", err)
 	}
-	fmt.Printf("Deleted document %v\n", deleteResult)
+	fmt.Printf("Deleted document: %v\n", deleteResult)
 
-	err = client.Disconnect(context.TODO())
+	err = client.Disconnect(ctx)
 	if err != nil {
-		panic(err)
+		log.Fatalf("could not dsconnect: %s\n", err)
 	}
 }
