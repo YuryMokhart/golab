31e67f46b879897fcbe1ee3543f51f9be06a8ab9
wsd-129-71b <wsd-129-71b@wsd-129-71bs-Mac-mini.local>
2019-07-09 14:39:03 +0300

    TODO minor fix

--- mongo/dbConn.go
+++ mongo/dbConn.go
@@ -2,30 +2,28 @@ package mongo
 
 import (
 	"context"
-	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// DBConnect connects to the database.
+// DBConnect connects to the mongo database.
 func DBConnect() *mongo.Collection {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
-		// TODO: who is responsible for fatalling?
-		log.Fatalf("could not connect mongoDB to a new client: %s\n", err)
+		return nil //, fmt.Errorf("could not create a new client to connect to the database: %s", err)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	err = client.Connect(ctx)
 	if err != nil {
-		log.Fatalf("could not initialise the client: %s\n", err)
+		return nil //, fmt.Errorf("could not create a new client to connect to the database: %s", err)
 	}
 	err = client.Ping(ctx, nil)
 	if err != nil {
-		log.Fatalf("could not ping: %s\n", err)
+		return nil //, fmt.Errorf("could not ping if a new client can connect to the database: %s", err)
 	}
 	collection := client.Database("tournament").Collection("user")
-	return collection
+	return collection //, nil
 }
