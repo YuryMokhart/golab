a5a2510ba3a5a10bfd0d71571755fe3493b99555
wsd-129-71b <wsd-129-71b@wsd-129-71bs-Mac-mini.local>
2019-07-09 15:26:31 +0300

    wrap errors in main

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ func main() {
 	r, err := handlers.Router()
 	if err != nil {
 		// TODO: don't forget to wrap your error
-		log.Fatal(err)
+		log.Fatalf("could not register a new route %s", err)
 	}
 	err = http.ListenAndServe(":8080", r)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("could not listen and serve %s", err)
 	}
 }
