7f4cdc3930fa85acc8f58d0a4aa1796c925a63c2
Yury Mokhart <yurymokhart@MacBook-Pro-Yury.local>
2019-07-11 12:07:34 +0300

    Add error wrapping.

--- controller/controller.go
+++ controller/controller.go
@@ -12,10 +12,10 @@ import (
 // TODO: you need that interface, but not in the controller.
 type controller interface {
 	// TODO: controller layer should do not know about mongo package.
-	CreateUser(entity.User) *mongoDriver.InsertOneResult
-	PrintUsers(entity.Users) entity.Users
-	FindUser(primitive.ObjectID) entity.User
-	DeleteUser(primitive.ObjectID)
+	CreateUser(entity.User) (*mongoDriver.InsertOneResult, error)
+	PrintUsers() (entity.Users, error)
+	FindUser(primitive.ObjectID) (entity.User, error)
+	DeleteUser(primitive.ObjectID) error
 }
 
 // ControllerStruct struct.
@@ -34,18 +34,28 @@ func (c ControllerStruct) CreateUser(user entity.User) (*mongoDriver.InsertOneRe
 }
 
 // PrintUsers returns all users from the database.
-func (c ControllerStruct) PrintUsers(users entity.Users) entity.Users {
-	users, _ = c.m.PrintUsers()
-	return users
+func (c ControllerStruct) PrintUsers() (entity.Users, error) {
+	users, err := c.m.PrintUsers()
+	if err != nil {
+		return nil, fmt.Errorf("could not print users: %s", err)
+	}
+	return users, nil
 }
 
 // FindUser finds a specific user by id.
-func (c ControllerStruct) FindUser(id primitive.ObjectID) entity.User {
-	user, _ := c.m.FindUser(id)
-	return user
+func (c ControllerStruct) FindUser(id primitive.ObjectID) (entity.User, error) {
+	user, err := c.m.FindUser(id)
+	if err != nil {
+		return user, fmt.Errorf("could not find a users: %s", err)
+	}
+	return user, nil
 }
 
 // DeleteUser deletes a specific user.
-func (c ControllerStruct) DeleteUser(id primitive.ObjectID) {
-	c.m.DeleteUser(id)
+func (c ControllerStruct) DeleteUser(id primitive.ObjectID) error {
+	err := c.m.DeleteUser(id)
+	if err != nil {
+		return fmt.Errorf("could not delete a users: %s", err)
+	}
+	return nil
 }
--- handlers/handle.go
+++ handlers/handle.go
@@ -23,72 +23,88 @@ type HTTPHandler struct {
 func Router() (*mux.Router, error) {
 	r := mux.NewRouter()
 	// TODO: change the naming chain: printHandler...
-	r.HandleFunc("/users", handlerPrint).Methods(http.MethodGet)
-	r.HandleFunc("/user", handlerPost).Methods(http.MethodPost)
-	r.HandleFunc("/user/{id}", handlerFind).Methods(http.MethodGet)
-	r.HandleFunc("/user/{id}", handlerDelete).Methods(http.MethodDelete)
+	r.HandleFunc("/users", printHandler).Methods(http.MethodGet)
+	r.HandleFunc("/user", postHandler).Methods(http.MethodPost)
+	r.HandleFunc("/user/{id}", findHandler).Methods(http.MethodGet)
+	r.HandleFunc("/user/{id}", deleteHandler).Methods(http.MethodDelete)
 
 	return r, nil
 }
 
-func handlerPrint(w http.ResponseWriter, r *http.Request) {
+func printHandler(w http.ResponseWriter, r *http.Request) {
 	var hh HTTPHandler
 	// TODO: think about content type for errors.
 	w.Header().Set("Content-Type", "application/json")
-	var users entity.Users
 	// TODO: where is your error, man?
-	users = hh.h.PrintUsers(users)
-	err := json.NewEncoder(w).Encode(users)
+	users, err := hh.h.PrintUsers()
+	if err != nil {
+		helpers.ErrorHelper(w, err, "could not print a user")
+
+		return
+	}
+	err = json.NewEncoder(w).Encode(users)
 	if err != nil {
 		// TODO: keep the same error design.
 		// TODO: you return errors for user, not for a programmer.
-		helpers.ErrorHelper(w, err, "couldn't encode users in handlerPrint")
+		helpers.ErrorHelper(w, err, "couldn't encode users in printHandler")
 		return
 	}
 }
 
-func handlerPost(w http.ResponseWriter, r *http.Request) {
+func postHandler(w http.ResponseWriter, r *http.Request) {
 	var hh HTTPHandler
 	w.Header().Set("Content-Type", "application/json")
 	var user entity.User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		helpers.ErrorHelper(w, err, "couldn't encode user in handlerPost")
+		helpers.ErrorHelper(w, err, "couldn't decode a user in postHandler")
 		return
 	}
-	result := hh.h.CreateUser(user)
+	result, err := hh.h.CreateUser(user)
+	if err != nil {
+		helpers.ErrorHelper(w, err, "could not create a user")
+		return
+	}
 	err = json.NewEncoder(w).Encode(result)
 	if err != nil {
-		helpers.ErrorHelper(w, err, "could not encode result in handlerPost")
+		helpers.ErrorHelper(w, err, "could not encode result in postHandler")
 		return
 	}
 }
 
-func handlerFind(w http.ResponseWriter, r *http.Request) {
+func findHandler(w http.ResponseWriter, r *http.Request) {
 	var hh HTTPHandler
 	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	id, err := primitive.ObjectIDFromHex(vars["id"])
 	if err != nil {
-		helpers.ErrorHelper(w, err, "hex string is not valid ObjectID in hanndlerFind")
+		helpers.ErrorHelper(w, err, "hex string is not valid ObjectID in findHandler")
 		return
 	}
-	user := hh.h.FindUser(id)
+	user, err := hh.h.FindUser(id)
+	if err != nil {
+		helpers.ErrorHelper(w, err, "could not find a user")
+		return
+	}
 	err = json.NewEncoder(w).Encode(user)
 	if err != nil {
-		helpers.ErrorHelper(w, err, "couldn't encode users in handlerFind")
+		helpers.ErrorHelper(w, err, "couldn't encode users in findHandler")
 		return
 	}
 }
 
-func handlerDelete(w http.ResponseWriter, r *http.Request) {
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	var hh HTTPHandler
 	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	id, err := primitive.ObjectIDFromHex(vars["id"])
 	if err != nil {
-		helpers.ErrorHelper(w, err, "hex string is not valid ObjectID in helperDelete")
+		helpers.ErrorHelper(w, err, "hex string is not valid ObjectID in deleteHandler")
 		return
 	}
-	hh.h.DeleteUser(id)
+	err = hh.h.DeleteUser(id)
+	if err != nil {
+		helpers.ErrorHelper(w, err, "could not delete a user")
+		return
+	}
 }
