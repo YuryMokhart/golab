b7db55faeab8e93486e090046d81c8dc332773ba
Yury Mokhart <yurymokhart@MacBook-Pro-Yury.local>
2019-07-19 11:06:40 +0300

    modify

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/YuryMokhart/golab/controller"
+	"github.com/YuryMokhart/golab/handler"
 	"github.com/YuryMokhart/golab/mongo"
 )
 
 func main() {
 	db := mongo.DBConnect()
 	controller := controller.New(db)
-	_ = controller
-	/*handler := &handlers.HTTPHandler{C: controller}
-	r, err := handlers.Router(handler)
+	httphandler := handler.New(controller)
+	r, err := handler.Router(httphandler)
 	if err != nil {
 		log.Fatalf("could not register a new route %s", err)
 	}
 	err = http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatalf("could not listen and serve %s", err)
-	}*/
+	}
 }
 
 // TODO: cover with unit tests all the code.
